64c44fca9cfd31ef841fea8a6b77dd1941066265
jenkins <jenkins@localhost.localdomain>
2019-06-26 19:49:17 -0400

    dd

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-WORKSPARCE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven-proj
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven-proj
 docker container run --rm -v /root/.m2:/root/.m2 -v $WORKSPACE/java-app:/app -w /app maven:3-alpine "$@"
